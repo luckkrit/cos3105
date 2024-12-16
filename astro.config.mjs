5619c1cde6db85f44004a7a81216cb47654761da
Krit Chomaitong <luckkrit@hotmail.com>
2024-12-16 23:48:52 +0700

    update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://luckkrit.github.io',
+	base: '/cos3105',
 	integrations: [
 		starlight({
 			title: 'COS3105',
