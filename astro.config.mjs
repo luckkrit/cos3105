e4a1fd05f7369a72e7689f03d942cceb8bcd298b
Krit Chomaitong <luckkrit@hotmail.com>
2024-12-16 23:22:52 +0700

    add lab2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,29 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://luckkrit.github.io',
+	base: 'cos3105',
 	integrations: [
 		starlight({
-			title: 'My Docs',
+			title: 'COS3105',
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				// github: 'https://github.com/withastro/starlight',
 			},
 			sidebar: [
+				// {
+				// 	label: 'Guides',
+				// 	items: [
+				// 		// Each item here is one entry in the navigation menu.
+				// 		{ label: 'Example Guide', slug: 'guides/example' },
+				// 	],
+				// },
+				// {
+				// 	label: 'Reference',
+				// 	autogenerate: { directory: 'reference' },
+				// },
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
+					label: 'Labs',
+					autogenerate: { directory: 'labs' },
 				},
 			],
 		}),
